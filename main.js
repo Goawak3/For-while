dc26f4548e551733fb3e87e270214db312fe9f2f
Dmitriy <flawy032@mail.ru>
2023-10-14 14:00:07 +0300

    don

---
+++ main.js
@@ -0,0 +1,20 @@
+for ( let z = 2; z <= 12; z = z + 2) {
+    console.log(z);
+};
+
+for (let i = 10; i > 0; i--) {
+    console.log(i)
+};
+
+let i = 0
+
+while(i < 12) {
+    i = i + 2
+    console.log(i)
+};
+let x = 11
+
+while (x > 1) {
+    x--
+    console.log(x)
+}
